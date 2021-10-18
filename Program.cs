017f0b44e50268f5157b6489474cbf6b382f1e3f
Brandon VanBuskirk <Brandonvb@hotmail.ca>
2021-10-18 12:56:13 -0300

    .

--- Program.cs
+++ Program.cs
@@ -11,8 +11,104 @@ namespace Loops
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Loops:");
 
+            Console.WriteLine("BEFORE Loops:");
+            Console.WriteLine("");
+
+
+            //display the numbers from 1-10
+
+            Console.WriteLine(1);
+            Console.WriteLine(2);
+            Console.WriteLine(3);
+            Console.WriteLine(4);
+            Console.WriteLine(5);
+            Console.WriteLine(6);
+            Console.WriteLine(7);
+            Console.WriteLine(8);
+            Console.WriteLine(9);
+            Console.WriteLine(10);
+
+
+            //display the numbers from 1-100
+            int count;
+            count = 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1; 
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+            Console.WriteLine(count); count = count + 1;
+
+
+            //display number from 1-n -- as a method
+            Console.WriteLine("AFTER WITH LOOPS");
+           
+            // display the numbers from 1-10 
+            // "for loop"
+            // will increase by 1 until it reaches 10. change the "10" to increase number.
+
+            // 1) declaration/initialization of
+            //2) stop condition
+            // 3) increment loop variable
+
+
+            for (int i = 1; i <=10; i = i + 1)
+            {
+                Console.WriteLine(i);
+              
+               
+            }
+
+
+
+
+
+
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+
+
+            }
+
+            Console.WriteLine("");
 
             Console.ReadKey();
         }
